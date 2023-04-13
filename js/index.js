096826228e7613c019810adfac8626962fde9893
HebaSakrH <128267375+HebaSakrH@users.noreply.github.com>
2023-04-13 15:06:32 +0200

    images more

--- js/index.js
+++ js/index.js
@@ -23,24 +23,24 @@ let bulletY =  0;
 
 //background
 const starsImg = new Image()
-starsImg.src = 'Images/space.png'
+starsImg.src = '/images/space.png'
 let starsX = 0;
 let stars2 = 0;
 //player 
 const playerImg = new Image()
-playerImg.src = 'images/pena2.png'
+playerImg.src = '/images/pena2.png'
 //obstecals draw
 const mushroomImg = new Image()
-mushroomImg.src = 'images/mushroom.png'
+mushroomImg.src = '/images/mushroom.png'
 
 const escobarImg = new Image()
 escobarImg.src = 'images/pablo.png'
 
 const beerImg = new Image()
-beerImg.src ='images/beer.png'
+beerImg.src ='/images/beer.png'
 
 const groguImg = new Image()
-groguImg.src ='images/grogu.png'
+groguImg.src ='/images/grogu.png'
 
 
 const gameSound = new Audio('sounds/theFoyer.wav')
